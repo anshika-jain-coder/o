7d8ce96c2b5e219a6ca518c3ce9c415aa676a4c7
anshika-jain-coder <73584808+anshika-jain-coder@users.noreply.github.com>
2020-12-18 18:54:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+canvas =
+ document.getElementById("mycanvas");
+ ctx= canvas.getContext("2d");
+ 
+ctx.beginPath();
+ ctx.strokeStyle="red";
+ ctx.linewidth=1;
+ ctx.rect(150,143,450,220);
+ ctx.stroke();
+
+ctx.beginPath();
+ ctx.strokeStyle="blue";
+ ctx.lineWidth=5;
+ ctx.arc(250,210,40,0,2 * Math.PI);
+ ctx.stroke();
+
+ctx.beginPath();
+ ctx.strokeStyle="black";
+ ctx.lineWidth=5;
+ ctx.arc(350,210,40,0,2 * Math.PI);
+ ctx.stroke();
+ 
+ctx.beginPath();
+ ctx.strokeStyle="red";
+ ctx.lineWidth=5;
+ ctx.arc(450,210,40,0,2 * Math.PI);
+ ctx.stroke();
+ 
+ctx.beginPath();
+ ctx.strokeStyle="yellow";
+ ctx.lineWidth=5;
+ ctx.arc(300,240,40,0,2 * Math.PI);
+ ctx.stroke();
+
+ctx.beginPath();
+ ctx.strokeStyle="green";
+ ctx.lineWidth=5;
+ ctx.arc(400,240,40,0,2 * Math.PI);
+ ctx.stroke();
